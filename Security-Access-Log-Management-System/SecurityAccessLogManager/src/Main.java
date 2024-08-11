356cb2ed5f8f53def2fdab5b2256d14771a7856a
jaredlaytonwilliams <147614358+jaredlaytonwilliams@users.noreply.github.com>
2024-08-11 14:44:40 -0500

    Delete Security-Access-Log-Management-System directory

--- Security-Access-Log-Management-System/SecurityAccessLogManager/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DatabaseManager.connect();
-        DatabaseManager.createTables();   
-        DatabaseManager.logAccessEvent(1, "Success"); }
-}
